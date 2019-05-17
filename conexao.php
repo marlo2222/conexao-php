9b95739a5be900f52e7942dc38133478e3d85192
marlo2222 <marloh2222@gmail.com>
2019-05-17 12:12:47 -0300

    adicionando configuraçoes sigleton

--- conexao.php
+++ conexao.php
@@ -1,3 +1,37 @@
 <?php
 
+class Conexao{
+    private static $pdo;
+
+    //escondendo o contrutor
+    private function __construct(){
+
+    }
+
+    public static function getInstance(){
+        if(!isset(self::$pdo)){
+            try {
+                $host = "";
+                $dbname = "";
+                $port = "";
+                $user = "";
+                $pass = "";
+
+                $opcoes = array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8',\PDO::ATTR_PERSISTENT => TRUE);
+
+                $dsn = "mysql:host={$host};dbname={$dbname};port={$port}";
+
+                self::$pdo = new \PDO($dsn, $user, $pass, $opcoes);
+                self::$pdo->setAttribute(
+                    \PDO::ATTR_ERRMODE,
+                    \PDO::ERRMODE_EXCEPTION
+                );
+            } catch (\PDOException $e) {
+                echo "ERRO" . $e->getMessage();
+            }
+        }
+        return self::$pdo;
+    }
+
+}
 ?>
